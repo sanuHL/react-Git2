49532259b195001e02289f23f210c90f39225543
sanuHL <sanuliyanaarachchi@gmail.com>
2022-03-02 22:00:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-7r5tnd
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-7r5tnd)
---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./style.css";
+///import "./App.css";
+import Profile from "./profile";
+
+export default function App() {
+  return (
+    <div>
+        <div className="App">
+      <header className="App-header">
+      </header>
+      <Profile />
+    </div>
+    </div>
+  );
+}
